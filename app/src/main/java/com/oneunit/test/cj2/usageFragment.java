8fdb9e0a559f1ade38af5220a0bf68450d34d697
Kurumbang <bishalkurumbang@live.com>
2015-10-28 10:09:06 +0100

    The graph layout is added

--- app/src/main/java/com/oneunit/test/cj2/usageFragment.java
+++ app/src/main/java/com/oneunit/test/cj2/usageFragment.java
@@ -1,18 +1,61 @@
 package com.oneunit.test.cj2;
 
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * Created by Bishal on 10/16/2015.
  */
 public class usageFragment extends Fragment {
 
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.usage_fragment,container,false);
+
+        spinner = (Spinner)view.findViewById(R.id.usage_dropdown);
+        adapter = ArrayAdapter.createFromResource(getActivity(),R.array.usage,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+"selected",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        GraphView graph = (GraphView)view.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6),
+                new DataPoint(12, 8)
+
+        });
+        graph.addSeries(series);
+
         return view;
     }
 }
